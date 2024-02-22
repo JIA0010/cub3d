9aed6860f73d60f5b318319114da54712e70a2a9
Yuina Hirai <yhirai@c1r15s2.42tokyo.jp>
2024-02-22 17:22:53 +0900

    map_closed

--- srcs/error/error_map.c
+++ srcs/error/error_map.c
@@ -6,14 +6,14 @@
 /*   By: yhirai <yhirai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 13:27:35 by yhirai            #+#    #+#             */
-/*   Updated: 2024/02/22 14:25:10 by yhirai           ###   ########.fr       */
+/*   Updated: 2024/02/22 16:13:06 by yhirai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/error.h"
-#include "../../includes/parser.h"
 
 static bool	check_map_closed(t_data *data);
+static bool	inside(char **map, size_t x, size_t z);
 
 bool	error_map(t_data *data)
 {
@@ -45,7 +45,50 @@ bool	error_map(t_data *data)
 
 static bool	check_map_closed(t_data *data)
 {
-	floodfill();
-	(void)data;
+	size_t	x;
+	size_t	z;
+
+	z = 0;
+	while (1)
+	{
+		x = 0;
+		if (data->parser->map[x][z] == '\0')
+			break ;
+		while (data->parser->map[x] != NULL)
+		{
+			if (data->parser->map[x][z] == '0'
+				&& inside(data->parser->map, x, z) == false)
+				return (false);
+			x++;
+		}
+		z++;
+	}
+	return (true);
+}
+
+static bool	inside(char **map, size_t x, size_t z)
+{
+	size_t	i;
+
+	i = 0;
+	while (map[x + i] != NULL && map[x + i][z] != '1')
+		i++;
+	if (map[x + i] == NULL)
+		return (false);
+	i = 0;
+	while (0 <= x + i && map[x + i][z] != '1')
+		i--;
+	if (x + i < 0)
+		return (false);
+	i = 0;
+	while (map[x][z + i] != '\0' && map[x][z + i] != '1')
+		i++;
+	if (map[x][z + i] == '\0')
+		return (false);
+	i = 0;
+	while (0 <= z + i && map[x][z + i] != '1')
+		i--;
+	if (z + i < 0)
+		return (false);
 	return (true);
 }
--- srcs/ft_free.c
+++ srcs/ft_free.c
@@ -6,29 +6,45 @@
 /*   By: yhirai <yhirai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:48:27 by yhirai            #+#    #+#             */
-/*   Updated: 2024/02/21 15:15:57 by yhirai           ###   ########.fr       */
+/*   Updated: 2024/02/22 17:03:44 by yhirai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+static void	ft_free_parser(t_data *data);
+
 void	ft_free(t_data *data)
+{
+	ft_free_parser(data);
+	if (data->player_pos)
+		free(data->player_pos);
+	if (data)
+		free(data);
+}
+
+static void	ft_free_parser(t_data *data)
 {
 	size_t	x;
 
 	x = 0;
-	free(data->parser->all);
-	free(data->parser->path_north);
-	free(data->parser->path_south);
-	free(data->parser->path_west);
-	free(data->parser->path_east);
+	if (data->parser->all)
+		free(data->parser->all);
+	if (data->parser->path_north)
+		free(data->parser->path_north);
+	if (data->parser->path_south)
+		free(data->parser->path_south);
+	if (data->parser->path_west)
+		free(data->parser->path_west);
+	if (data->parser->path_east)
+		free(data->parser->path_east);
 	while (data->parser->map[x] != NULL)
 	{
 		free(data->parser->map[x]);
 		x++;
 	}
-	free(data->parser->map);
-	free(data->parser);
-	free(data->player_pos);
-	free(data);
+	if (data->parser->map)
+		free(data->parser->map);
+	if (data->parser)
+		free(data->parser);
 }
